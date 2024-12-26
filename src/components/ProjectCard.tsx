2151c1e174abd34622b7faec39961272cd7991a2
Steph 88 <78787829+spottier88@users.noreply.github.com>
2024-12-26 11:31:52 +0100

    Update ProjectCard.tsx

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -62,7 +62,9 @@ export const ProjectCard = ({
             <Star className="h-4 w-4 text-yellow-500" aria-label="Suivi DGS" />
           )}
           <CardTitle className="text-xl font-semibold">{title}</CardTitle>
-        
+        {suivi_dgs && (
+            <Star className="h-4 w-4 text-yellow-500" aria-label="Suivi DGS" />
+          )}
         </div>
         <div className="flex items-center gap-2">
           <Button
