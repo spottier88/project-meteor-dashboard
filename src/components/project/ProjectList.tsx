ea966d6823991d2bb9089d002acf38ce243e7fa3
Steph 88 <78787829+spottier88@users.noreply.github.com>
2025-01-26 20:43:51 +0100

    Update ProjectList.tsx

--- src/components/project/ProjectList.tsx
+++ src/components/project/ProjectList.tsx
@@ -42,7 +42,6 @@ export const ProjectList = ({
           onProjectReview={onProjectReview}
           onViewHistory={onViewHistory}
           onProjectDeleted={onProjectDeleted}
-          onFilteredProjectsChange={onFilteredProjectsChange}
         />
       )}
     </>
